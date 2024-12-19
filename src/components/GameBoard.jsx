87a27016de596c8d7fd56c38b195b054d9e1b983
HemlockSip <davide.tansini87@gmail.com>
2024-12-19 17:08:15 +0100

    adjusted visibility on the Game Board and added state management on parent component

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -7,24 +7,25 @@ const initialGameBoard = [
 ];
 
 // this is to avoid hardcoding the game board inside the component
-export default function GameBoard(){
+export default function GameBoard({onSelectSquare, activeSymbol}){
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
     function handleSelectSquare(rowIndex, colIndex){
         setGameBoard((prevGameBoard)=> {
             const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = 'X';
+            updatedBoard[rowIndex][colIndex] = activeSymbol;
             return updatedBoard;
-        }  );
+        });
+        onSelectSquare();
     }
 
     return <ol id="game-board">
-      {initialGameBoard.map((row,rowIndex)=> 
+      {gameBoard.map((row,rowIndex)=> 
        <li key={rowIndex}>
         <ol>
             {row.map((playerSymbol, colIndex) => 
              <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={()=> handleSelectSquare(rowIndex,colIndex)} >{playerSymbol}</button>
             </li>)}
         </ol>
       </li> )}    
