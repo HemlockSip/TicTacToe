5bfc963fd3ffff45ceb8531d5ffb7f8113083f62
HemlockSip <davide.tansini87@gmail.com>
2024-12-20 12:40:14 +0100

    added log component and state management

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -7,17 +7,26 @@ const initialGameBoard = [
 ];
 
 // this is to avoid hardcoding the game board inside the component
-export default function GameBoard({onSelectSquare, activeSymbol}){
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, turns }){
+ let gameBoard = initialGameBoard;
 
-    function handleSelectSquare(rowIndex, colIndex){
-        setGameBoard((prevGameBoard)=> {
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = activeSymbol;
-            return updatedBoard;
-        });
-        onSelectSquare();
+    for (const turn of turns){
+        const {square, player}  = turn;
+        const {row, col} = square;
+
+        gameBoard[row][col] = player;
     }
+
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    // function handleSelectSquare(rowIndex, colIndex){
+    //     setGameBoard((prevGameBoard)=> {
+    //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+    //         updatedBoard[rowIndex][colIndex] = activeSymbol;
+    //         return updatedBoard;
+    //     });
+    //     onSelectSquare();
+    // }
 
     return <ol id="game-board">
       {gameBoard.map((row,rowIndex)=> 
@@ -25,7 +34,7 @@ export default function GameBoard({onSelectSquare, activeSymbol}){
         <ol>
             {row.map((playerSymbol, colIndex) => 
              <li key={colIndex}>
-                <button onClick={()=> handleSelectSquare(rowIndex,colIndex)} >{playerSymbol}</button>
+                <button onClick={()=> onSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
             </li>)}
         </ol>
       </li> )}    
