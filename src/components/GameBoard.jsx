dba9bca45e2399fb1005473a0a9567537b3fc9cb
HemlockSip <davide.tansini87@gmail.com>
2024-12-29 12:43:45 +0100

    added management state to restart the game. still broken tho.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,21 +1,10 @@
 import { useState } from "react";
 
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-];
+
 
 // this is to avoid hardcoding the game board inside the component
-export default function GameBoard({ onSelectSquare, turns }){
- let gameBoard = initialGameBoard;
-
-    for (const turn of turns){
-        const {square, player}  = turn;
-        const {row, col} = square;
-
-        gameBoard[row][col] = player;
-    }
+export default function GameBoard({ onSelectSquare, board }){
+ 
 
     // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
@@ -29,12 +18,15 @@ export default function GameBoard({ onSelectSquare, turns }){
     // }
 
     return <ol id="game-board">
-      {gameBoard.map((row,rowIndex)=> 
+      {board.map((row,rowIndex)=> 
        <li key={rowIndex}>
         <ol>
             {row.map((playerSymbol, colIndex) => 
              <li key={colIndex}>
-                <button onClick={()=> onSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+                <button onClick={()=> onSelectSquare(rowIndex,colIndex)} 
+                disabled={playerSymbol ==! null}>
+               
+                {playerSymbol}</button>
             </li>)}
         </ol>
       </li> )}    
